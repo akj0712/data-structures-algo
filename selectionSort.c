fe30e1c4af397a86e5514f800c016d8c7779ba9c
Abhinav Kumar Jha <1ds20cs003@dsce.edu.in>
2022-01-04 18:21:15 +0530

    selection sort

---
+++ selectionSort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void selectionSort(int array[], int n)
+{
+    int i, j, min_ind;
+    for ( i = 0; i < n - 1; i++) {
+        min_ind = i;
+        for ( j = i + 1; j < n; j++)
+            if (array[j] < array[min_ind])
+                min_ind = j;
+
+            swap(&array[min_ind], &array[i]);
+    }
+}
+
+void printArray(int array[], int size)
+{
+    for (int i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+int main (void)
+{
+    int array[] = {20, 10, 30, 50, 80, 40, 60};
+    int n = sizeof(array) / sizeof(array[0]);
+    selectionSort(array, n);
+    printf("Sorted array is:\n");
+    printArray(array, n);
+
+
+    return 0;
+}
